85b4bad93c72588efc9a153d2024bb1db1cc0f0d
venkat <venkyms@gmail.com>
2019-05-26 14:25:21 +0530

    - API documentation using swagger config - use ApiModel annotation to provide documentation for model class

--- src/main/java/com/venkyms/rest/webservices/resfulwebservices/SwaggerConfig.java
+++ src/main/java/com/venkyms/rest/webservices/resfulwebservices/SwaggerConfig.java
@@ -2,16 +2,38 @@ package com.venkyms.rest.webservices.resfulwebservices;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2);
-    }
+  public static final Contact DEFAULT_CONTACT = new Contact("Venkat", "NA", "NA");
+  public static final ApiInfo DEFAULT_API_INFO =
+      new ApiInfo(
+          "Rest API Documentation",
+          "Rest API Documentation with spring boot and swagger",
+          "1.0",
+          "urn:tos",
+          DEFAULT_CONTACT,
+          "Apache 2.0",
+          "http://www.apache.org/licenses/LICENSE-2.0");;
+  private static final Set<String> DEFAULT_PRODUCES_CONSUMES =
+      new HashSet<>(Arrays.asList("application/json", "application/xml"));
+
+  @Bean
+  public Docket api() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .apiInfo(DEFAULT_API_INFO)
+        .consumes(DEFAULT_PRODUCES_CONSUMES)
+        .produces(DEFAULT_PRODUCES_CONSUMES);
+  }
 }
--- src/main/java/com/venkyms/rest/webservices/resfulwebservices/user/User.java
+++ src/main/java/com/venkyms/rest/webservices/resfulwebservices/user/User.java
@@ -1,16 +1,22 @@
 package com.venkyms.rest.webservices.resfulwebservices.user;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@ApiModel(description = "User Data model")
 public class User {
   private Integer id;
 
   @Size(min = 2, message = "Invalid name")
+  @ApiModelProperty(notes = "Name should be minimum 2 char")
   private String name;
 
   @Past
+  @ApiModelProperty(notes = "Date should be in the past")
   private Date birthDate;
 
   public User(Integer id, String name, Date birthDate) {
